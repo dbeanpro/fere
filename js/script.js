0c6a2256ec5fae753e24043a305b02f0cfc8b1d5
Dallas Bean <174537881+dbeanpro@users.noreply.github.com>
2024-08-19 12:05:18 -0700

    First table completed. Overarching classes established. function reveal() developed.

--- js/script.js
+++ js/script.js
@@ -5,8 +5,14 @@ document.addEventListener("DOMContentLoaded", function() {
         textarea.style.height = 'auto';
         textarea.style.height = `${textarea.scrollHeight}px`;
     }
-    
+
     document.querySelectorAll('textarea').forEach(textarea => {
         textarea.addEventListener('input', () => adjustHeight(textarea));
     })
 })
+
+function reveal(elID) {
+    let target = document.getElementById(elID);
+
+    target.classList.toggle("hidden");
+}
