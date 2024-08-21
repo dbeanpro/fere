47e2366659862af5ed4a7fe0fd83599b41231d0d
Dallas Bean <174537881+dbeanpro@users.noreply.github.com>
2024-08-21 11:57:04 -0700

    Complete overhaul of textareas (ie, removed and replaced with divs). Deleted textarea function from js file. Deleted weatherbox class. Created textAreaAlt class for the replacement divs.

--- js/script.js
+++ js/script.js
@@ -1,16 +1,3 @@
-// textArea dynamics:
-document.addEventListener("DOMContentLoaded", function() {
-
-    function adjustHeight(textarea) {
-        textarea.style.height = 'auto';
-        textarea.style.height = `${textarea.scrollHeight}px`;
-    }
-
-    document.querySelectorAll('textarea').forEach(textarea => {
-        textarea.addEventListener('input', () => adjustHeight(textarea));
-    })
-})
-
 function reveal(elID) {
     let target = document.getElementById(elID);
 
